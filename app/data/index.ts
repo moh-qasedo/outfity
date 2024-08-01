418a91d50479a6346424f8f1e4a3c01a754c3eec
Mohammed Qasedo <qasedo2000@gmail.com>
2024-08-01 22:57:36 +0300

    add slides data

---
+++ app/data/index.ts
@@ -0,0 +1,36 @@
+import {SlideModel} from '../models';
+
+export const slides: SlideModel[] = [
+  {
+    title: 'Relaxed',
+    subtitle: 'Find Your Outfits',
+    description:
+      'Confused about your outfit? Don’t worry! Find the best outfit here!',
+    color: '#BFEAF5',
+    right: false,
+  },
+  {
+    title: 'Playful',
+    subtitle: 'Hear it First, Wear',
+    description:
+      'Hating the clothes in your wardrobe? Explore hundreds of outfit ideas',
+    color: '#BEECC4',
+    right: true,
+  },
+  {
+    title: 'Excentric',
+    subtitle: 'Your Style, Your Way',
+    description:
+      ' Create your individual & unique style and look amazing everyday',
+    color: '#FFE4D9',
+    right: false,
+  },
+  {
+    title: 'Funky',
+    subtitle: 'Look Good, Feel Good',
+    description:
+      'Discover the latest trends in fashion and explore your personality',
+    color: '#FFDDDD',
+    right: true,
+  },
+];
