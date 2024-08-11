b36897971db723343b558e6c5c2490e6dc67a671
Mohammed Qasedo <qasedo2000@gmail.com>
2024-08-11 23:09:41 +0300

    create Button component

---
+++ app/components/Button/index.tsx
@@ -0,0 +1,78 @@
+import React, {memo, useMemo} from 'react';
+import {Pressable, StyleSheet, TextStyle, View, ViewStyle} from 'react-native';
+import Text from '../Text';
+import {Varient} from '../../types';
+import CONSTANTS from '../../constants';
+
+type Props = {
+  borderRadius?: number;
+  label: string;
+  labelStyle?: TextStyle;
+  onPress: () => void;
+  style?: ViewStyle;
+  varient: Varient;
+};
+
+const Button = ({
+  borderRadius = 100,
+  label,
+  labelStyle,
+  onPress,
+  style,
+  varient = CONSTANTS.VARIENT.PRIMARY,
+}: Props) => {
+  const {containerStyle, textStyle} = useMemo(
+    () => ({
+      textStyle: StyleSheet.flatten([
+        {
+          fontFamily: CONSTANTS.FONT_FAMILIES.LATO_BOLD,
+          color:
+            varient == CONSTANTS.VARIENT.PRIMARY
+              ? CONSTANTS.COLORS.WHITE
+              : CONSTANTS.COLORS.BLACK,
+        },
+        labelStyle,
+      ]),
+      containerStyle: StyleSheet.flatten([
+        {
+          borderRadius: borderRadius,
+          backgroundColor:
+            varient == CONSTANTS.VARIENT.PRIMARY
+              ? CONSTANTS.COLORS.GREEN
+              : CONSTANTS.COLORS.LIGHT_GRAY,
+        },
+        style,
+      ]),
+    }),
+    [borderRadius, labelStyle, style, varient],
+  );
+  return (
+    <View style={containerStyle}>
+      <Pressable
+        onPress={onPress}
+        style={({pressed}) => [
+          {
+            ...styles.pressable,
+            borderRadius,
+            backgroundColor: pressed
+              ? CONSTANTS.COLORS.BLACK10
+              : CONSTANTS.COLORS.TRANSPARENT,
+          },
+        ]}>
+        <Text style={textStyle}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default memo(Button);
+
+const styles = StyleSheet.create({
+  pressable: {
+    width: '100%',
+    paddingHorizontal: 50,
+    paddingVertical: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
