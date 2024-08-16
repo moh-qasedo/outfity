ee83d3eda18efa61fc50f730f7eb8d1a108d7cbb
Mohammed Qasedo <qasedo2000@gmail.com>
2024-08-17 01:42:36 +0300

    hide status bar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import React, {memo} from 'react';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import Screen from './app/components/Screen';
 import AuthenticationStackNavigator from './app/navigators/AuthenticationStackNavigator';
+import CONSTANTS from './app/constants';
 
 const App = (): React.JSX.Element => {
   return (
-    <NavigationContainer>
-      <AuthenticationStackNavigator />
-    </NavigationContainer>
+    <Screen>
+      <StatusBar
+        translucent={true}
+        backgroundColor={CONSTANTS.COLORS.TRANSPARENT}
+      />
+      <NavigationContainer>
+        <AuthenticationStackNavigator />
+      </NavigationContainer>
+    </Screen>
   );
 };
 
