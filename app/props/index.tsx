04e712a4ee6d7d12f4070661c978aaa402d9efc3
Mohammed Qasedo <qasedo2000@gmail.com>
2024-08-21 14:26:23 +0300

    add props module

---
+++ app/props/index.tsx
@@ -0,0 +1,22 @@
+import {ColorValue, TextStyle, ViewStyle} from 'react-native';
+import {IconType, Varient} from '../types';
+
+export type IconProps = {
+  type: IconType;
+  name: string;
+  size: number;
+  color: ColorValue;
+  backgroundColor?: ColorValue;
+  style?: ViewStyle;
+};
+
+export type ButtonProps = {
+  onPress: () => void;
+  borderRadius?: number;
+  label?: string;
+  labelStyle?: TextStyle;
+  style?: ViewStyle;
+  contentStyle?: ViewStyle;
+  icon?: IconProps;
+  varient?: Varient;
+};
